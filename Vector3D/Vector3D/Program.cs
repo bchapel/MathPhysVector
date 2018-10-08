d23419a20c3dfd9a27a1ddfcf676d9d6d2df8b43
bchapel <bchapel@stumail.jccc.edu>
2018-10-08 05:19:16 -0500

    Initial COmmit of MathPhys Vector3D class.

---
+++ Vector3D/Vector3D/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Vector3D
+{
+    class Vector3D
+    {
+
+
+        //Stored X, Y, Z, coordinates of this Vector.
+        private float X;
+        private float Y;
+        private float Z;
+        private float W = 1;
+
+        private float pitch;
+        private float heading;
+
+        //Initializing the X, Y, and Z values for this vector. 
+        public Vector3D(float initX, float initY, float initZ)
+        {
+            X = initX;
+            Y = initY;
+            Z = initZ;
+        }
+
+        //Set the X, Y, and Z values of the 
+        public void SetRectGivenRect(float inputX, float inputY, float inputZ)
+        {
+            X = inputX;
+            Y = inputY;
+            Z = inputZ;
+        }
+        //Print the X, Y, and Z coordinate of this vector.
+        public void PrintRect()
+        {
+            Console.WriteLine("(" + X + ", " + Y + ", " + Z + ")");
+        }
+        //Print the magnitude of this vector. Square Root of ( x*x + y*y + z*z)
+        public void PrintMag()
+        {
+            float magnitude = GetMag();
+            Console.WriteLine("Magnitude: " + magnitude);
+        }
+        //Return the private X value.
+        public float GetX()
+        {
+            return X;
+        }
+        //Return the private Y value.
+        public float GetY()
+        {
+            return Y;
+        }
+        //Return the private Z value.
+        public float GetZ()
+        {
+            return Z;
+        }
+
+        //Calculate and return the magnitude of the vector.
+        public float GetMag()
+        {
+
+            return (float)Math.Sqrt((X * X) + (Y * Y) + (Z * Z));
+        }
+
+        //Calculate and return the SQUARED magnitude of the vector.
+        public float GetMagSq()
+        {
+            return (X * X) + (Y * Y) + (Z * Z);
+        }
+
+
+        public void SetRectGivenPolar(float mag, double angle)
+        {
+            double tempX = mag * Math.Cos(angle);
+            double tempY = mag * Math.Sin(angle);
+
+
+            X = (float)tempX;
+            Y = (float)tempY;
+            //Polar form is 2D!
+            Z = 0;
+        }
+
+        public void SumRect(float x, float y, float z)
+        {
+            X += x;
+            Y += y;
+            Z += z;
+        }
+
+        //Subtracts the input values from the Vector.
+        public void SubtractRect(float x, float y, float z)
+        {
+            X -= x;
+            Y -= y;
+            Z -= z;
+        }
+
+        //Multiples the rect by the input values.
+        public void MultiplyRect(float input)
+        {
+            X *= input;
+            Y *= input;
+            Z *= input;
+        }
+
+        public void SetRectGivenMagHeadPitch(float mag, double heading, double pitch)
+        {
+            if (mag < 0)
+                mag = mag * -1;
+
+            double tempX = mag * Math.Cos(heading) * Math.Cos(pitch);
+            double tempY = mag * Math.Sin(heading) * Math.Cos(pitch);
+            double tempZ = mag * Math.Sin(pitch);
+
+            //tempX = Math.Round(tempX, 3);
+            //tempY = Math.Round(tempY, 3);
+            //tempZ = Math.Round(tempY, 3);
+
+            X = (float)tempX;
+            Y = (float)tempY;
+            Z = (float)tempZ;
+        }
+
+        public void SumRectGivenMagHeadPitch(float mag, double heading, double pitch)
+        {
+            if (mag < 0)
+                mag = mag * -1;
+
+            double tempX = mag * Math.Cos(heading) * Math.Cos(pitch);
+            double tempY = mag * Math.Sin(heading) * Math.Cos(pitch);
+            double tempZ = mag * Math.Sin(pitch);
+
+            //tempX = Math.Round(tempX, 3);
+            //tempY = Math.Round(tempY, 3);
+            //tempZ = Math.Round(tempY, 3);
+
+            X += (float)tempX;
+            Y += (float)tempY;
+            Z += (float)tempZ;
+        }
+
+        public void PrintPolar()
+        {
+            PrintMag();
+            //Calculating Angle for Polar form
+            Console.WriteLine("Angle: " + Math.Tan(X / Y));
+        }
+
+        public void PrintMagheadPitch()
+        {
+            Console.WriteLine("Pitch: " + GetPitch());
+            Console.WriteLine("Heading: " + GetHeading());
+            Console.WriteLine("Magnitude: " + GetMag());
+        }
+
+        public void PrintDirection()
+        {
+            Console.WriteLine("Alpha: " + GetAlpha());
+            Console.WriteLine("Beta: " + GetBeta());
+            Console.WriteLine("Gamma: " + GetGamma());
+        }
+
+
+        public float GetPitch()
+        {
+            double tempPitch = Z / GetMag();
+            tempPitch = Math.Sin(tempPitch);
+            return (float)tempPitch;
+        }
+
+        //GetHeading needs to be modified to make X on left.
+        public float GetHeading()
+        {
+
+            double tempHeading = X / Math.Sqrt(X * X + Y * Y);
+            tempHeading = Math.Cos(tempHeading);
+            return (float)tempHeading;
+        }
+
+        public float GetAlpha()
+        {
+            float alpha = 1;
+            return alpha;
+        }
+
+        public float GetBeta()
+        {
+            float beta = 1;
+            return beta;
+        }
+
+        public float GetGamma()
+        {
+            float gamma = 1;
+            return gamma;
+        }
+
+        public float DegreeToRad(float degree)
+        {
+            float rad = degree * (float)Math.PI / 180;
+
+            return rad;
+        }
+
+        public float RadtoDegrees(float rad)
+        {
+            float degrees = 180 / (float)Math.PI;
+            return degrees;
+        }
+
+    }
+}
